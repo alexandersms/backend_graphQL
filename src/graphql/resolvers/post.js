051da072525014063c98d04302e11c2a42b04f6a
Alex Simisi Nta <alex.simisinta@fanprime.io>
2021-10-29 03:06:33 +0200

    install husky and git precommit checks, create docs

--- src/graphql/resolvers/post.js
+++ src/graphql/resolvers/post.js
@@ -1,21 +1,8 @@
 export default {
   Query: {
-    getAllPosts: () => {
-      return [
-        {
-          title: "Hello world",
-          content: "This is simple content",
-        },
-        {
-          title: "J'ai pris ma décision",
-          content: "This is simple content",
-        },
-        {
-          title:
-            "Donner une chance à une personne qui t'a trahi c'est lui donner une 2ème balle pour te tuer",
-          content: "This is simple content",
-        },
-      ];
+    getAllPosts: async (_, {}, { Post }) => {
+      let posts = await Post.find();
+      return posts;
     },
   },
   Mutation: {
